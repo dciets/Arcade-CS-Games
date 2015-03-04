5995fb2af60149c5092e23ba8be5397f7132afb3
MaDumont <dumont.marcantoine13@gmail.com>
2015-03-03 22:27:45 -0500

    Reverting minigame init file

--- minigames/__init__.py
+++ minigames/__init__.py
@@ -1,8 +1,8 @@
 # from stest import STest
 # from mtest import MTest
-#from blackcat import BlackCat
-#from spingame import SpinGame
-#from mirrors.minigame import MirrorsMinigame
-#from shoot_targets.shoot_targets import ShootTargets
-#from leprechaunBeatemUp.leprechaunBeatemUp import LeprechaunBeatemUp
+from blackcat import BlackCat
+from spingame import SpinGame
+from mirrors.minigame import MirrorsMinigame
+from shoot_targets.shoot_targets import ShootTargets
+from leprechaunBeatemUp.leprechaunBeatemUp import LeprechaunBeatemUp
 from cutlery_fall.cutlery_fall import CutleryFall
