cbb6f1ff7f38c54b4c216dfa6dd3a92e0078ed4a
Gabriel Kenderessy <g@brie.lk>
2015-03-11 23:23:08 -0400

    Bees WIP

--- minigames/__init__.py
+++ minigames/__init__.py
@@ -1,9 +1,10 @@
-from blackcat import BlackCat
-from spingame import SpinGame
-from mirrors.minigame import MirrorsMinigame
-from shoot_targets.shoot_targets import ShootTargets
-from leprechaunBeatemUp.leprechaunBeatemUp import LeprechaunBeatemUp
-from cutlery_fall.cutlery_fall import CutleryFall
-from its_raining_beer.its_raining_beer import ItsRainingBeer
-from crackgame import CrackGame
-from chiotteclean import ChiotteGame
+# from blackcat import BlackCat
+# from spingame import SpinGame
+# from mirrors.minigame import MirrorsMinigame
+# from shoot_targets.shoot_targets import ShootTargets
+# from leprechaunBeatemUp.leprechaunBeatemUp import LeprechaunBeatemUp
+# from cutlery_fall.cutlery_fall import CutleryFall
+# from its_raining_beer.its_raining_beer import ItsRainingBeer
+# from crackgame import CrackGame
+# from chiotteclean import ChiotteGame
+from ladders.minigame import LaddersMinigame
--- minigames/ladders/minigame.py
+++ minigames/ladders/minigame.py
@@ -1,27 +1,58 @@
+from itertools import cycle
 import pygame
+import random
+from pygame.rect import Rect
 from minigames import minigame
 
 
 class LaddersMinigame(minigame.Minigame):
     name = "Evade the bees!"
     game_type = minigame.MULTIPLAYER
-    max_duration = 50000
+    max_duration = 10000
 
     BEE_BASE_SPEED = 10
+    BEE_BASE_COUNT = 6
 
     def __init__(self, game):
         minigame.Minigame.__init__(self, game)
 
         self.game = game
 
-        self.bg1 = pygame.image.load("./res/img/ladders/bg1.png")
-        self.bg2 = pygame.image.load("./res/img/ladders/bg2.png")
-        self.bg3 = pygame.image.load("./res/img/ladders/bg3.png")
-        self.ladder = pygame.image.load("./res/img/ladders/ladder.png")
+        # Background GFX
+        self.gfx_bg1 = pygame.image.load("./res/img/ladders/bg1.png")
+        self.gfx_bg2 = pygame.image.load("./res/img/ladders/bg2.png")
+        self.gfx_bg3 = pygame.image.load("./res/img/ladders/bg3.png")
+        self.gfx_ladder = pygame.image.load("./res/img/ladders/ladder.png")
+
+        # Entities GFX
+        self.gfx_left_bee = cycle([
+            pygame.transform.flip(pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((0, 0), (47, 49))), True),
+            pygame.transform.flip(pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((54, 0), (47, 49))), True),
+            pygame.transform.flip(pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((108, 0), (47, 49))), True),
+            pygame.transform.flip(pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((54, 0), (47, 49))), True)
+        ])
+        self.gfx_right_bee = cycle([
+            pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((0, 0), (47, 49))),
+            pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((54, 0), (47, 49))),
+            pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((108, 0), (47, 49))),
+            pygame.image.load("./res/img/ladders/bee.png").subsurface(Rect((54, 0), (47, 49)))
+        ])
+
+        # Entities
+        self.bees = ([], [])
 
     def init(self):
-        pass
+        for i in range(LaddersMinigame.BEE_BASE_COUNT / 2):
+            self.bees[0].append((self.gfx_left_bee, random.randint(150, 500)))
+            self.bees[1].append((self.gfx_left_bee, random.randint(150, 500)))
 
     def tick(self):
-        self.game.screen.blit(self.bg2, (0, 0))
-        self.game.screen.blit(self.bg1, (0, 0))
+        # if self.frame % 2 == 0:
+        #     self.bees = self.gfx_bee.next()
+
+        self.game.screen.blit(self.gfx_bg2, (-2 * (self.frame % 319), 0))
+        self.game.screen.blit(self.gfx_bg1, (20, 0))
+        self.game.screen.blit(self.gfx_ladder, (self.game.GAME_WIDTH / 2 - 75, 100))
+        self.game.screen.blit(self.gfx_ladder, (self.game.GAME_WIDTH / 2 + 75, 100))
+        # self.game.screen.blit(self.bees, (100, 100))
+        self.game.screen.blit(self.gfx_bg3, (-8 * (self.frame % 159), 0))
