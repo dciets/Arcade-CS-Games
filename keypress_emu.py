27866bed5eb4ca7975770011be1fa0f1a0ab8f42
Becojo <benoit.cote.jodoin@gmail.com>
2017-03-11 16:37:01 -0500

    handle outputs

--- keypress_emu.py
+++ keypress_emu.py
@@ -1,23 +1,35 @@
 import serial
 from serial.tools import list_ports
 from pykeyboard import PyKeyboard
+import sys
+import select
 
 ports = list_ports.comports()
+
 k = PyKeyboard()
 keys = 'W A S D E U H J K I'.split()
 
 if ports:
-    ser = serial.Serial(ports[0].device, 115200)
+    ser = serial.Serial(ports[0].device, 115200, timeout=0)
 
     while True:
-        c = ord(ser.read())
-        index = c >> 1
-        state = c & 1
+        char = ser.read()
 
-        if state == 1:
-            k.press_key(keys[index])
-        else:
-            k.release_key(keys[index])
+        if char:
+            code = ord(char)
+            index = code >> 1
+            state = code & 1
+
+            if state == 1:
+                k.press_key(keys[index])
+            else:
+                k.release_key(keys[index])
+
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            command = sys.stdin.readline().strip()
+
+            if command:
+                ser.write(command[0])
 
 else:
     print 'No serial ports found'
