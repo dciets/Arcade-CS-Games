69ad970e403b868458d134f49d0fdca3dd10f2b1
Becojo <benoit.cote.jodoin@gmail.com>
2017-03-21 20:59:09 -0400

    added real controller mapping for reference

--- input_map.py
+++ input_map.py
@@ -12,6 +12,13 @@ PLAYERS_MAPPING = [
     [K_UP, K_RIGHT, K_DOWN, K_LEFT, K_0], # UP, RIGHT,  DOWN, LEFT, ACTION
 ]
 
+# Real mapping
+# PLAYERS_MAPPING = [
+#     [K_w, K_d, K_s, K_a, K_SPACE], # UP, RIGHT,  DOWN, LEFT, ACTION
+#     [K_UP, K_RIGHT, K_DOWN, K_LEFT, K_0], # UP, RIGHT,  DOWN, LEFT, ACTION
+# ]
+
+
 def get_player_keys(player):
     keys = pygame.key.get_pressed()
     return [keys[k] for k in PLAYERS_MAPPING[player]]
